16dac59787e5b734333086eab08383d2118988b2
davennemenzo <davennemenzo@gmail.com>
2025-02-06 00:18:30 +0800

    About Page Unfinished Work

--- src/components/Hero.vue
+++ src/components/Hero.vue
@@ -1,21 +1,23 @@
 <template>
-  <div class="h-screen flex flex-col bg-cover items-center">
-    <div class="flex flex-col overflow-hidden">
+    <div
+    class="min-h-[300px] max-h-[300px] md:min-h-[600px] md:max-h-[600px] lg:h-screen flex flex-col bg-cover overflow-hidden"
+  >
+    <div class="flex flex-col justify-center overflow-hidden relative h-full">
       <!-- UI/UX DESIGNER Sliding from Left to Right -->
       <div
-        class="flex flex-1 justify-center items-center text-[60px] sm:text-[150px] md:text-[160px] lg:text-[200px] text-lavender font-bold uppercase animate-slide-left-right whitespace-nowrap leading-tight mt-10 sm:mt-0"
+        class="flex justify-center items-center text-[90px] sm:text-[150px] md:text-[160px] lg:text-[240px] text-lavender font-bold uppercase animate-slide-left-right whitespace-nowrap leading-none"
       >
         UI/UX DESIGNER
       </div>
 
       <!-- FRONTEND DEVELOPER Sliding from Right to Left -->
       <div
-        class="flex flex-1 justify-center items-center text-[60px] sm:text-[150px] md:text-[160px] lg:text-[240px] text-lavender font-bold uppercase animate-slide-right-left whitespace-nowrap leading-tight"
+        class="flex justify-center items-center text-[90px] sm:text-[150px] md:text-[160px] lg:text-[240px] text-lavender font-bold uppercase animate-slide-right-left whitespace-nowrap leading-none"
       >
         FRONTEND DEVELOPER
       </div>
       <div
-        class="absolute mt-10 inset-0 mx-auto z-50 w-[280px] h-[280px] md:w-[400px] md:h-[400px] lg:w-[600px] lg:h-[600px] flex justify-center"
+        class="absolute bottom-0 left-1/2 transform -translate-x-1/2 z-50 w-[300px] h-[300px] md:w-[540px] md:h-[540px] lg:w-[600px] lg:h-[600px] flex justify-center"
       >
         <img
           src="@/images/test.png"
@@ -28,14 +30,15 @@
 </template>
 
 <script>
-import Navbar from "@/components/Navbar.vue";
+
 
 // Import the CSS file from the style folder
 import "@/styles/animation.css";
+import PageContainer from './PageContainer.vue';
 
 export default {
   components: {
-    Navbar,
+    
   },
 };
 </script>
--- src/components/PageContainer.vue
+++ src/components/PageContainer.vue
@@ -1,6 +1,6 @@
 <template>
   <div
-    class="container mx-auto px-4 sm:px-6 md:px-8 lg:px-10"
+    class="container mx-auto px-4 sm:px-6 md:px-8 lg:px-20"
     style="max-width: 2980px"
   >
     <slot />
