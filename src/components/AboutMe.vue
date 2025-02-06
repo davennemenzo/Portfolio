ba08e710640726633357dd9b75cd5f469640d3ad
davennemenzo <davennemenzo@gmail.com>
2025-02-07 00:19:41 +0800

    Added Projects to the Home Page

--- src/components/AboutMe.vue
+++ src/components/AboutMe.vue
@@ -1,48 +1,47 @@
 <template>
-    <div class="h-[300px] md:min-h-[600px] md:max-h-[600px] lg:h-screen flex flex-col bg-lavender overflow-hidden">
+  <div class="flex flex-col bg-cover h-auto">
     <!-- Header section -->
     <PageContainer>
       <div class="flex justify-center items-center flex-col relative mb-12">
-      <div
-        class="mt-6 text-[35px] sm:text-[50px] md:text-[55px] lg:text-[60px] text-cover font-bold uppercase whitespace-nowrap leading-none"
-      >
-        <span>About Me</span>
-      </div>
-    </div>
-
-    <!-- Content section -->
-    <div
-      class="container mx-auto  flex flex-col md:flex-row justify-between items-start gap-8"
-    >
-      <!-- Text content -->
-      <div class="flex flex-col space-y-4 md:w-1/2">
-        <span class="text-2xl font-bold">HELLO</span>
-        <span class="text-3xl font-bold mb-4">I'M DAVEN</span>
-        <span class="text-lg">
-          I'm a UI/UX Designer and Frontend Developer passionate about crafting
-          seamless, intuitive, and visually compelling digital experiences. With
-          a keen eye for design and a deep understanding of code, I bridge
-          aesthetics with functionality—turning ideas into interactive,
-          user-friendly interfaces that engage and inspire.
-        </span>
+        <div
+          class="mt-6 text-[35px] sm:text-[50px] md:text-[55px] lg:text-[60px] text-lavender font-bold uppercase whitespace-nowrap leading-none"
+        >
+          <span>About Me</span>
+        </div>
       </div>
 
-      <!-- Image section -->
+      <!-- Content section -->
       <div
-        class=" w-[300px] h-[300px] md:w-[540px] md:h-[540px] lg:w-[600px] lg:h-[600px]"
+        class="container mx-auto flex flex-col md:flex-row justify-between items-start gap-8"
       >
-        <img
-          src="@/images/test.png"
-          alt="About"
-          class="w-full h-full object-cover"
-        />
+        <!-- Text content -->
+        <div class="flex flex-col space-y-4 md:w-1/2 text-lavender">
+          <span class="text-2xl font-bold">HELLO</span>
+          <span class="text-3xl font-bold mb-4">I'M DAVEN</span>
+          <span class="text-lg">
+            I'm a UI/UX Designer and Frontend Developer passionate about
+            crafting seamless, intuitive, and visually compelling digital
+            experiences. With a keen eye for design and a deep understanding of
+            code, I bridge aesthetics with functionality—turning ideas into
+            interactive, user-friendly interfaces that engage and inspire.
+          </span>
+        </div>
+
+        <!-- Image section -->
+        <div
+          class="w-[300px] h-[300px] md:w-[540px] md:h-[540px] lg:w-[600px] lg:h-[600px] -mt-8 md:-mt-12 lg:-mt-13"
+        >
+          <img
+            src="@/images/test.png"
+            alt="About"
+            class="w-full h-full object-cover"
+          />
+        </div>
       </div>
-    </div>
     </PageContainer>
   </div>
 </template>
 
 <script setup>
-import PageContainer from './PageContainer.vue';
-
+import PageContainer from "./PageContainer.vue";
 </script>
