bde0c27ca94efb62cf25edeb6f51e6cb53a3e25e
davennemenzo <davennemenzo@gmail.com>
2025-02-25 02:16:35 +0800

    Fixed Bug in the About Me PAge

---
+++ src/components/SkillTag.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="p-[2px] bg-gradient-to-r from-blue-500 to-emerald-500 rounded-full w-fit">
+  <div class="p-3 bg-white rounded-full text-xs">
+    {{ skill }}
+  </div>
+</div>
+</template>
+
+<script setup>
+const props = defineProps({
+  skill: String,
+  isHighlighted: {
+    type: Boolean,
+    default: false,
+  },
+});
+</script>
+
+<style scoped>
+</style>
